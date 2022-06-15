91618cf610fb1af68aaa5e82e1d2587541992f60
jcselorio <joemarie@ubx.ph>
2022-06-15 16:35:44 +0800

    done wallet repo test

--- src/components/wallets/walletsRepository.js
+++ src/components/wallets/walletsRepository.js
@@ -2,7 +2,7 @@ require('app-module-path').addPath(require('app-root-path').toString());
 require('dotenv').config();
 
 const knex = require('knex')(require('knexfile'));
-
+const {isString, isNumber, isEmpty, isObject} = require('lodash');
 
 const logger = require('src/utilities/loggerUtil');
 
@@ -23,6 +23,10 @@ async function createNewWallet(
   const METHOD = '[createNewWallet]';
   logger.info(`${TAG} ${METHOD}`);
 
+  if (!isString(id)) throw new Error('invalid uuid');
+  if (!isString(userId)) throw new Error('invalid user id');
+  if (!isNumber(amount)) throw new Error('invalid amount');
+
   const newWallet = {
     uuid: id,
     user_id: userId,
@@ -42,14 +46,21 @@ async function getWalletByUuid(
   const METHOD = '[getWalletByUuid]';
   logger.info(`${TAG} ${METHOD}`);
 
-  const wallet = await knex(WALLETS_TABLE).where({uuid}).first();
+  if (!isString(uuid)) throw new Error('invalid uuid');
 
-  return {
-    uuid: wallet.uuid,
-    amount: wallet.amount,
-    createdAt: wallet.created_at,
-    lastUpdatedAt: wallet.last_updated_at,
-  };
+  const wallet = await knex.where({uuid}).from(WALLETS_TABLE).first();
+
+  if (wallet) {
+    return {
+      uuid: wallet.uuid,
+      amount: wallet.amount,
+      userId: wallet.user_id,
+      createdAt: wallet.created_at,
+      lastUpdatedAt: wallet.last_updated_at,
+    };
+  }
+
+  return false;
 }
 
 /**
@@ -62,15 +73,21 @@ async function getWalletByUserId(
   const METHOD = '[getWalletByUserId]';
   logger.info(`${TAG} ${METHOD}`);
 
+  if (!isString(userId)) throw new Error('invalid uuid');
+
   const wallet = await knex
       .where({user_id: userId})
       .from(WALLETS_TABLE)
       .first();
 
+  console.log(wallet);
   if (wallet) {
     return {
-      userId: wallet.user_id,
+      uuid: wallet.uuid,
       amount: wallet.amount,
+      userId: wallet.user_id,
+      createdAt: wallet.created_at,
+      lastUpdatedAt: wallet.last_updated_at,
     };
   }
 
@@ -90,15 +107,25 @@ async function updateWalletByUserId(
   const METHOD = '[updateWalletByUserId]';
   logger.info(`${TAG} ${METHOD}`);
 
+  if (!isString(userId)) throw new Error('invalid uuid');
+  if (Object.keys(dataToUpdate).length === 0) {
+    throw new Error('invalid properties');
+  }
+
+
   const wallet = await knex
       .where({user_id: userId})
       .update(dataToUpdate)
       .from(WALLETS_TABLE);
 
-  return {
-    uuid: wallet.uuid,
-    amount: wallet.amount,
-  };
+  if (wallet) {
+    return {
+      uuid: wallet.uuid,
+      amount: wallet.amount,
+    };
+  }
+
+  return false;
 }
 
 module.exports = {
--- tests/unit/repositories/walletsRepository.test.js
+++ tests/unit/repositories/walletsRepository.test.js
@@ -8,7 +8,7 @@ const sinon = require('sinon');
 const proxyquire = require('proxyquire');
 
 const fixture = require('tests/unit/repositories/' +
-  'fixtures/usersRepositoryConfig');
+  'fixtures/walletsRepositoryConfig');
 
 const {stub} = sinon;
 chai.use(chaiAsPromised);
@@ -25,7 +25,7 @@ describe('walletsRepository', () => {
   const updateStub = stub();
 
   afterEach(() => {
-    knexStub.resetBehavior();
+    knexStub.reset();
     insertStub.reset();
     intoStub.reset();
     whereStub.reset();
@@ -34,11 +34,11 @@ describe('walletsRepository', () => {
     updateStub.reset();
   });
 
-  describe.only('#createNewWallet', () => {
+  describe('#createNewWallet', () => {
     knexStub.returns({insert: insertStub});
     insertStub.returns({into: intoStub});
 
-    const usersRepositoryProxy = proxyquire(
+    const walletRepositoryProxy = proxyquire(
         'src/components/wallets/walletsRepository', {
           'knex': knexStub,
         });
@@ -47,7 +47,7 @@ describe('walletsRepository', () => {
     ' provided with valid arguments', async () => {
       intoStub.resolves(fixture.createWalletResponse);
 
-      const result = await usersRepositoryProxy.createNewWallet(
+      const result = await walletRepositoryProxy.createNewWallet(
           fixture.id,
           fixture.userId,
           fixture.amount,
@@ -63,13 +63,186 @@ describe('walletsRepository', () => {
       expect(result).to.deep.equal(fixture.createWalletResponse);
     });
     it('should throw an if provided with invalid uuid', async () => {
-      await expect(usersRepositoryProxy.createNewUser(
+      await expect(walletRepositoryProxy.createNewWallet(
           null,
-          fixture.email,
-          fixture.firstName,
-          fixture.lastName,
-          fixture.password,
+          fixture.userId,
+          fixture.amount,
       )).to.eventually.rejectedWith(/invalid uuid/);
+    });
+    it('should throw an if provided with invalid user id', async () => {
+      await expect(walletRepositoryProxy.createNewWallet(
+          fixture.id,
+          null,
+          fixture.amount,
+      )).to.eventually.rejectedWith(/invalid user id/);
+    });
+    it('should throw an if provided with invalid user id', async () => {
+      await expect(walletRepositoryProxy.createNewWallet(
+          fixture.id,
+          fixture.userId,
+          null,
+      )).to.eventually.rejectedWith(/invalid amount/);
+    });
+  });
+
+  describe('#getWalletByUuid', () => {
+    knexStub.returns({where: whereStub});
+
+    beforeEach(() => {
+      whereStub.returns({from: fromStub});
+      fromStub.returns({first: firstStub});
+    });
+
+    const walletRepositoryProxy = proxyquire(
+        'src/components/wallets/walletsRepository', {
+          'knex': knexStub,
+        });
+
+    it('should retrieve wallet details if'+
+    ' provided with valid arguments', async () => {
+      firstStub.resolves(fixture.getWalletDetailsResponse);
+
+      const result = await walletRepositoryProxy.getWalletByUuid(
+          fixture.id,
+      );
+      const wallet = {
+        uuid: fixture.getWalletDetailsResponse.uuid,
+        amount: fixture.getWalletDetailsResponse.amount,
+        userId: fixture.getWalletDetailsResponse.user_id,
+        createdAt: fixture.getWalletDetailsResponse.created_at,
+        lastUpdatedAt: fixture.getWalletDetailsResponse.last_updated_at,
+      };
+
+      expect(whereStub.calledOnceWithExactly({
+        uuid: fixture.id})).to.be.true;
+      expect(fromStub.calledOnceWithExactly(WALLETS_TABLE)).to.be.true;
+      expect(result).to.deep.equal(wallet);
+    });
+
+    it('should return false if wallet is not found', async () => {
+      firstStub.resolves(undefined);
+
+      const result = await walletRepositoryProxy.getWalletByUuid(
+          fixture.id,
+      );
+
+      expect(result).to.be.false;
+    });
+    it('should throw an if provided with invalid uuid', async () => {
+      await expect(walletRepositoryProxy.getWalletByUuid(
+          null,
+      )).to.eventually.rejectedWith(/invalid uuid/);
+    });
+  });
+
+  describe('#getWalletByUserId', () => {
+    knexStub.returns({where: whereStub});
+
+    beforeEach(() => {
+      whereStub.returns({from: fromStub});
+      fromStub.returns({first: firstStub});
+    });
+
+    const walletRepositoryProxy = proxyquire(
+        'src/components/wallets/walletsRepository', {
+          'knex': knexStub,
+        });
+
+    it('should retrieve wallet details if'+
+    ' provided with valid arguments', async () => {
+      firstStub.resolves(fixture.getWalletDetailsResponse);
+
+      const result = await walletRepositoryProxy.getWalletByUserId(
+          fixture.userId,
+      );
+      const wallet = {
+        uuid: fixture.getWalletDetailsResponse.uuid,
+        amount: fixture.getWalletDetailsResponse.amount,
+        userId: fixture.getWalletDetailsResponse.user_id,
+        createdAt: fixture.getWalletDetailsResponse.created_at,
+        lastUpdatedAt: fixture.getWalletDetailsResponse.last_updated_at,
+      };
+
+      expect(whereStub.calledOnceWithExactly({
+        user_id: fixture.userId})).to.be.true;
+      expect(fromStub.calledOnceWithExactly(WALLETS_TABLE)).to.be.true;
+      expect(result).to.deep.equal(wallet);
+    });
+    it('should return false if wallet is not found', async () => {
+      firstStub.resolves(undefined);
+
+      const result = await walletRepositoryProxy.getWalletByUserId(
+          fixture.userId,
+      );
+
+      expect(result).to.be.false;
+    });
+    it('should throw an if provided with invalid uuid', async () => {
+      await expect(walletRepositoryProxy.getWalletByUserId(
+          null,
+      )).to.eventually.rejectedWith(/invalid uuid/);
+    });
+  });
+
+  describe('#updateWalletByUserId', () => {
+    knexStub.returns({where: whereStub});
+
+    beforeEach(() => {
+      whereStub.returns({update: updateStub});
+      updateStub.returns({from: fromStub});
+    });
+
+    const walletRepositoryProxy = proxyquire(
+        'src/components/wallets/walletsRepository', {
+          'knex': knexStub,
+        });
+
+    it('should update wallet details if'+
+    ' provided with valid arguments', async () => {
+      fromStub.resolves(fixture.updateWalletResponse);
+
+      const result = await walletRepositoryProxy.updateWalletByUserId(
+          fixture.userId,
+          {
+            amount: 3000,
+          },
+      );
+
+      expect(whereStub.calledOnceWithExactly({
+        user_id: fixture.userId})).to.be.true;
+      expect(updateStub.calledOnceWithExactly( {
+        amount: 3000,
+      })).to.be.true;
+      expect(fromStub.calledOnceWithExactly(WALLETS_TABLE)).to.be.true;
+      expect(result).to.deep.equal(fixture.updateWalletResponse);
+    });
+    it('should return false if no wallet found', async () => {
+      fromStub.resolves(undefined);
+
+      const result = await walletRepositoryProxy.updateWalletByUserId(
+          fixture.userId,
+          {
+            amount: 3000,
+          },
+      );
+
+      expect(result).to.be.false;
+    });
+    it('should throw an if provided with invalid uuid', async () => {
+      await expect(walletRepositoryProxy.updateWalletByUserId(
+          null,
+          {
+            amount: 3000,
+          },
+      )).to.eventually.rejectedWith(/invalid uuid/);
+    });
+    it('should throw an if provided with invalid uuid', async () => {
+      await expect(walletRepositoryProxy.updateWalletByUserId(
+          fixture.userId,
+          {
+
+          },
+      )).to.eventually.rejectedWith(/invalid properties/);
     });
   });
 });
