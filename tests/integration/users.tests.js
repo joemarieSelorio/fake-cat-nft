6f4b2d9486f13a779bea16930feb1315839aec3f
jcselorio <joemarie@ubx.ph>
2022-06-17 00:22:42 +0800

    test

--- tests/integration/users.tests.js
+++ tests/integration/users.tests.js
@@ -4,16 +4,8 @@ require('dotenv').config();
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const bcrypt = require('bcrypt');
+const {v4: uuidv4} = require('uuid');
 const app = require('app');
-const {
-  createNewUser,
-  getUserByEmail,
-} = require('src/components/users/usersRepository');
-const {
-  createNewWallet,
-  getWalletByUuid,
-} = require('src/components/wallets/walletsRepository');
-const auth = require('src/middlewares/authorizationMiddleware');
 const knex = require('knex')(require('knexfile'));
 const fixture = require('tests/integration/fixtures/usersConfig');
 
@@ -23,12 +15,16 @@ chai.use(chaiHttp);
 const {
   USERS_TABLE,
   WALLETS_TABLE,
+  ASSETS_TABLE,
 } = process.env;
 
 describe('users', () => {
+  before(async ()=> {
+    await knex.raw(`SET foreign_key_checks = 0;`);
+  });
+
   describe('/post/users', () => {
-    before(async () => {
-      await knex.raw(`SET foreign_key_checks = 0;`);
+    after(async () => {
       await knex.raw(`TRUNCATE TABLE ${USERS_TABLE};`);
     });
     it('should create new user if'+
@@ -45,34 +41,35 @@ describe('users', () => {
     });
   });
 
-  describe.only('/users/:id/wallets', () => {
+  describe('/users/:id/wallets', () => {
     let loginResponse;
+    let uuid;
     after(async () => {
-      await knex.raw(`SET foreign_key_checks = 0;`);
       await knex.raw(`TRUNCATE TABLE ${USERS_TABLE};`);
       await knex.raw(`TRUNCATE TABLE ${WALLETS_TABLE};`);
     });
     before(async ()=> {
       const hashedPassword= await bcrypt.hash(fixture.password, 10);
+      uuid = uuidv4();
       await knex.insert({
-        uuid: fixture.id,
-        email_address: fixture.email,
+        uuid: uuid,
+        email_address: fixture.email1,
         first_name: fixture.firstName,
         last_name: fixture.lastName,
         password: hashedPassword,
       }).into(USERS_TABLE);
 
       await knex.insert({
-        uuid: fixture.id,
-        user_id: fixture.id,
-        amount: 1000,
+        uuid: uuid,
+        user_id: uuid,
+        amount: fixture.amount,
       }).into(WALLETS_TABLE);
 
       loginResponse = await chai
           .request(app)
           .post(`/login`)
           .send({
-            email: fixture.email,
+            email: fixture.email1,
             password: fixture.password,
           });
     });
@@ -80,9 +77,53 @@ describe('users', () => {
       const token = loginResponse.body.token;
       const response = await chai
           .request(app)
-          .get(`/users/${fixture.id}/wallets`)
-          .set('Authorization', 'Bearer ' + token)
-          .send(fixture.userRequestBody);
+          .get(`/users/${uuid}/wallets`)
+          .set('Authorization', 'Bearer ' + token);
+      expect(response.body.status).to.be.equal(200);
+    });
+  });
+
+  describe('/users/:id/assets', () => {
+    let loginResponse;
+    let uuid;
+    after(async () => {
+      await knex.raw(`TRUNCATE TABLE ${USERS_TABLE};`);
+      await knex.raw(`TRUNCATE TABLE ${ASSETS_TABLE};`);
+    });
+    before(async ()=> {
+      uuid = uuidv4();
+      const hashedPassword= await bcrypt.hash(fixture.password, 10);
+      await knex.insert({
+        uuid,
+        email_address: fixture.email2,
+        first_name: fixture.firstName,
+        last_name: fixture.lastName,
+        password: hashedPassword,
+      }).into(USERS_TABLE);
+
+      await knex.insert({
+        uuid,
+        name: fixture.assetName,
+        img_url: fixture.imgUrl,
+        user_id: fixture.id,
+        initial_amount: fixture.amount,
+        current_amount: fixture.amount,
+      }).into(ASSETS_TABLE);
+
+      loginResponse = await chai
+          .request(app)
+          .post(`/login`)
+          .send({
+            email: fixture.email2,
+            password: fixture.password,
+          });
+    });
+    it('should retrieve user assets', async () => {
+      const token = loginResponse.body.token;
+      const response = await chai
+          .request(app)
+          .get(`/users/${uuid}/assets`)
+          .set('Authorization', 'Bearer ' + token);
       expect(response.body.status).to.be.equal(200);
     });
   });
