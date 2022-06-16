1b50d8bada88069d89528252f762bbb0583c9d65
jcselorio <joemarie@ubx.ph>
2022-06-16 10:53:13 +0800

    done docker setup

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const {MYSQL_HOST, MYSQL_PORT,
-  MYSQL_ROOT_USER, MYSQL_PASSWORD, MYSQL_DB, MYSQL_MAX_POOL} = process.env;
+  MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB, MYSQL_MAX_POOL} = process.env;
 module.exports = {
   client: 'mysql',
   connection: {
     host: MYSQL_HOST,
     port: MYSQL_PORT,
-    user: MYSQL_ROOT_USER,
+    user: MYSQL_USER,
     password: MYSQL_PASSWORD,
     database: MYSQL_DB,
   },
